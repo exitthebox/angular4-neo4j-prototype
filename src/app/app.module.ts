f8026b8ddff4b90d63d3da1d8f178c67e9b4df0c
exitthebox <csurams@hotmail.com>
2017-06-14 07:42:49 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { FieldComponent } from './edit/field/field.component';
+
+const appRoutes: Routes=[
+  {path: 'add', component: EditComponent},
+  {path: 'edit', component: AddComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AddComponent,
+    EditComponent,
+    FieldComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
